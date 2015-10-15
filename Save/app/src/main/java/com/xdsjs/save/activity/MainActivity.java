ad56dafb125ee963675b6bcc05b764928bde3dee
buzuiweng101 <xdsjs001@126.com>
2015-10-16 00:12:53 +0800

    增加抽屉布局

--- Save/app/src/main/java/com/xdsjs/save/activity/MainActivity.java
+++ Save/app/src/main/java/com/xdsjs/save/activity/MainActivity.java
@@ -1,24 +1,45 @@
 package com.xdsjs.save.activity;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import com.special.ResideMenu.ResideMenu;
+import com.special.ResideMenu.ResideMenuItem;
 import com.xdsjs.save.R;
-import com.xdsjs.save.utils.TimeUtils;
 
 
 public class MainActivity extends BaseActivity {
 
-    private TextView tvContent;
+    private ResideMenu resideMenu;
+    private ResideMenuItem resideMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvContent = (TextView) findViewById(R.id.tv_content);
-        tvContent.setText(TimeUtils.getPrettyTime(Long.valueOf(TimeUtils.getFirstDayTimeOfWeek()))
-                        + "\n" + TimeUtils.getPrettyTime(Long.valueOf(TimeUtils.getFirstDayTimeOfMonth()))
-                        + "\n" + TimeUtils.getPrettyTime(Long.valueOf(TimeUtils.getFirstDayTimeOfYear()))
-        );
+        initView();
     }
+
+    private void initView(){
+        resideMenu = new ResideMenu(this);
+        resideMenu = new ResideMenu(this);
+        resideMenu.setBackground(R.drawable.person);
+        resideMenu.attachToActivity(this);
+        resideMenu.setMenuListener(menuListener);
+        //valid scale factor is between 0.0f and 1.0f. leftmenu'width is 150dip.
+        resideMenu.setScaleValue(0.6f);
+
+    }
+
+    private ResideMenu.OnMenuListener menuListener = new ResideMenu.OnMenuListener() {
+        @Override
+        public void openMenu() {
+            Toast.makeText(MainActivity.this, "Menu is opened!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void closeMenu() {
+            Toast.makeText(MainActivity.this, "Menu is closed!", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
--- Save/app/src/main/java/com/xdsjs/save/application/MyApplication.java
+++ Save/app/src/main/java/com/xdsjs/save/application/MyApplication.java
@@ -2,24 +2,29 @@ package com.xdsjs.save.application;
 
 import android.app.Application;
 
+import com.xdsjs.save.controller.MyController;
 import com.xdsjs.save.utils.LogUtils;
 
 /**
  * 在这里完成app的初始化
  * Created by xdsjs on 2015/10/13.
  */
-public class MyApplication extends Application{
+public class MyApplication extends Application {
 
     private static MyApplication myApplication = null;
+
+    private MyController myController = new MyController();
+
     @Override
     public void onCreate() {
         super.onCreate();
         myApplication = this;
         //设置是否打印日志,默认即为true
         LogUtils.isDebug = true;
+        myController.onInit(this);
     }
 
-    public static MyApplication getInstance(){
+    public static MyApplication getInstance() {
         return myApplication;
     }
 
