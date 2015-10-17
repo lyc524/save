e9a6792bf70e04baf6397394ed9002ac18adefe7
buzuiweng101 <xdsjs001@126.com>
2015-10-17 21:54:47 +0800

    添加ToggleButton

--- Save/app/src/main/java/com/xdsjs/save/activity/MainActivity.java
+++ Save/app/src/main/java/com/xdsjs/save/activity/MainActivity.java
@@ -1,17 +1,10 @@
 package com.xdsjs.save.activity;
 
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.special.ResideMenu.ResideMenu;
-import com.special.ResideMenu.ResideMenuItem;
 import com.xdsjs.save.R;
 
 
 public class MainActivity extends BaseActivity {
-
-    private ResideMenu resideMenu;
-    private ResideMenuItem resideMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,25 +14,7 @@ public class MainActivity extends BaseActivity {
     }
 
     private void initView(){
-        resideMenu = new ResideMenu(this);
-        resideMenu = new ResideMenu(this);
-        resideMenu.setBackground(R.drawable.person);
-        resideMenu.attachToActivity(this);
-        resideMenu.setMenuListener(menuListener);
-        //valid scale factor is between 0.0f and 1.0f. leftmenu'width is 150dip.
-        resideMenu.setScaleValue(0.6f);
 
     }
 
-    private ResideMenu.OnMenuListener menuListener = new ResideMenu.OnMenuListener() {
-        @Override
-        public void openMenu() {
-            Toast.makeText(MainActivity.this, "Menu is opened!", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void closeMenu() {
-            Toast.makeText(MainActivity.this, "Menu is closed!", Toast.LENGTH_SHORT).show();
-        }
-    };
 }
---
+++ Save/app/src/main/java/com/xdsjs/save/activity/PersonalInfoActivity.java
@@ -0,0 +1,17 @@
+package com.xdsjs.save.activity;
+
+import android.os.Bundle;
+
+import com.xdsjs.save.R;
+
+/**
+ * 个人信息界面
+ * Created by xdsjs on 2015/10/17.
+ */
+public class PersonalInfoActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info);
+    }
+}
